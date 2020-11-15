3e95d27068d59f15e2c544e0634e58580b8c94d7
Pablo Bastidas <pablo.bastidas@leaseplan.com>
2020-11-15 15:21:35 +0100

    First commit, pub integration

---
+++ src/features/drywall_calculation/presentation/components/Form.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+
+</template>
+
+<script>
+export default {
+  name: "Form"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/features/drywall_calculation/presentation/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <Form />
+</template>
+
+<script>
+import Form from "@/features/drywall_calculation/presentation/components/Form";
+export default {
+  name: "Main",
+  components: {Form}
+}
+</script>
+
+<style scoped>
+
+</style>
